248e1ed4608f5cccbf6482c5a7efcd18b184ca81
yu@sec.com <yu@sec.com>
2017-11-23 10:37:43 +0800

    add swagger service +

--- server/src/controllers/swagger_controller.ts
+++ server/src/controllers/swagger_controller.ts
@@ -1,11 +1,7 @@
-import { POST, GET,PUT, DELETE, BodyParam, PathParam, BaseController } from 'webapi-router';
+import { POST, GET,PUT, DELETE, BodyParam, PathParam, BaseController,Param } from 'webapi-router';
 import { ResObject } from '../common/res_object';
 import * as Koa from 'koa';
 import { RecordService } from '../services/record_service';
-import { RecordRunner } from '../run_engine/record_runner';
-import { DtoRecord } from '../interfaces/dto_record';
-import { DtoRecordRun } from '../interfaces/dto_record_run';
-import { DtoRecordSort } from '../interfaces/dto_record_sort';
 import { SessionService } from '../services/session_service';
 import {DtoSwaggerCache} from "../interfaces/dto_swagger_cache";
 import {SwaggerService} from "../services/swagger_service";
@@ -15,12 +11,12 @@ export default class SwaggerController extends BaseController {
     @POST('/swagger/:id')
     async createOrUpdate(ctx: Koa.Context, @BodyParam sc: DtoSwaggerCache): Promise<ResObject> {
         const user = SessionService.getUser(ctx);
-        return await RecordService.create(RecordService.fromDto(record), user);
+        return await SwaggerService.save(SwaggerService.fromDto(sc), user);
     }
     @GET('/swagger/:id')
-    async get(ctx: Koa.Context, @BodyParam record: DtoSwaggerCache): Promise<ResObject> {
+    async get(ctx: Koa.Context,@PathParam('id') id: string){
         const user = SessionService.getUser(ctx);
-        return await SwaggerService.create(RecordService.fromDto(record), user);
+        return await SwaggerService.getLast(id);
     }
 
     @DELETE('/swagger/:id')
@@ -28,24 +24,25 @@ export default class SwaggerController extends BaseController {
         return await RecordService.delete(id);
     }
 
-    @GET('/swagger/refresh')
-    async refresh(ctx: Koa.Context, @BodyParam record: DtoRecord): Promise<ResObject> {
+    @POST('/swagger/refresh')
+    async refresh(ctx: Koa.Context, @BodyParam data: DtoSwaggerCache) {
         const user = SessionService.getUser(ctx);
-        return await RecordService.update(RecordService.fromDto(record), user);
+        const record = SwaggerService.fromDto(data);
+        return await SwaggerService.refreshByUrlOrId(record, user);
     }
 
-    @POST('/swagger/history')
-    async history(ctx: Koa.Context, @BodyParam data: DtoRecordRun) {
-        const record = RecordService.fromDto(data.record);
+    @POST('/swagger/history/:historyId')
+    async history(ctx: Koa.Context, @PathParam('historyId') historyId: string,@BodyParam data: DtoSwaggerCache) {
+        const record = SwaggerService.fromDto(data);
         const userId = SessionService.getUserId(ctx);
-        return await RecordRunner.runRecordFromClient(record, data.environment, userId, ctx.res);
+        return await SwaggerService.getByHistory(record,historyId);
     }
 
     @GET('/swagger/init')
-    async buildRemote(ctx: Koa.Context, @BodyParam data: DtoRecordRun) {
-        const record = RecordService.fromDto(data.record);
+    async buildRemote(ctx: Koa.Context, @BodyParam data: DtoSwaggerCache) {
+        const record = SwaggerService.fromDto(data);
         const userId = SessionService.getUserId(ctx);
-        return await RecordRunner.runRecordFromClient(record, data.environment, userId, ctx.res);
+        return await SwaggerService.initByUrl(record);
     }
 
 
--- server/src/services/swagger_service.ts
+++ server/src/services/swagger_service.ts
@@ -1,52 +1,61 @@
 import { StringUtil } from '../utils/string_util';
-import { Variable } from '../models/variable';
-import { DtoVariable } from '../interfaces/dto_variable';
+
 import {ConnectionManager} from "./connection_manager";
 import {SwaggerCache} from "../models/swagger_cache";
 import {ResObject} from "../common/res_object";
 import {Message} from "../common/message";
+import {DtoSwaggerCache} from "../interfaces/dto_swagger_cache";
+import {DtoUser} from "../interfaces/dto_user";
+import {User} from "../models/user";
 
 export class SwaggerService {
 
 
-
-    static fromDto(dtoVariable: DtoVariable) {
-        const variable = dtoVariable as Variable;
+    static fromDto(dto: DtoSwaggerCache) {
+        const variable = dto as SwaggerCache;
         variable.id = variable.id || StringUtil.generateUID();
         return variable;
     }
-    static async getSwaggerJsonHistory(history:number,id:string){
 
-    }
-
-    static async getLastedSwaggerJsonById(id: string): Promise<Record> {
-
-        return record;
-    }
-
-    static async applyVariable(envId: string, content: string): Promise<string> {
-
-    }
-    static async saveSwagger(){
+    static async save(swagger: SwaggerCache,user:DtoUser): Promise<ResObject> {
         const connection = await ConnectionManager.getInstance();
 
-    }
-    static async save(swagger: SwaggerCache): Promise<ResObject> {
-        const connection = await ConnectionManager.getInstance();
         await connection.getRepository(SwaggerCache).persist(swagger);
 
         return { success: true, message: Message.projectSaveSuccess };
     }
 
-    static async updateProject(dtoProject: DtoProject): Promise<ResObject> {
+    static async updateCache(cache: SwaggerCache): Promise<ResObject> {
         const connection = await ConnectionManager.getInstance();
 
-        await connection.getRepository(Project)
-            .createQueryBuilder('project')
-            .where('id=:id', { id: dtoProject.id })
-            .update({ name: dtoProject.name, note: dtoProject.note })
+        await connection.getRepository(SwaggerCache)
+            .createQueryBuilder('swagger_cache')
+            .where('id=:id', { id: cache.id })
+            .update({ content: cache.content, updateDate: cache.updateDate })
             .execute();
 
         return { success: true, message: Message.projectSaveSuccess };
+    }
+    static async getLast(id:string){
+        const connection = await ConnectionManager.getInstance();
+
+        const cache = await connection.getRepository(SwaggerCache)
+            .createQueryBuilder('swagger_cache')
+            .where(`user.id = :id`)
+            .setParameter('id', id)
+            .getOne();
+        return cache
+    }
+
+    static getByHistory(record: SwaggerCache, historyId: string) {
+        
+    }
+
+    static initByUrl(record: SwaggerCache) {
+        
+    }
+
+    static refreshByUrlOrId(record: SwaggerCache, user: User) {
+
     }
 }
